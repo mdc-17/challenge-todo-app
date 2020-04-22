d9f758393a7be28e85e936d742a7979242f8b9af
Miguel Dominguez Chamorro <mdchamorro7@gmail.com>
2020-04-22 13:40:12 +0200

    Edit Working and Refreshing

--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -1,48 +1,45 @@
-import React, { useState, useEffect }from 'react'
-import axios from "axios"
-
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function EditTodo(props) {
   const [title, settitle] = useState(props.theTodo.title);
   const [body, setbody] = useState(props.theTodo.body);
-  
+
   const gettodoInfo = async () => {
-    const me = await axios.get(`http://localhost:4000/api/v1/todos/${props.theTodo._id}`);
-    settitle(me.data.title)
-    setbody(me.data.body)
-    
-  }
+    const me = await axios.get(
+      `http://localhost:4000/api/v1/todos/${props.theTodo._id}`
+    );
+    settitle(me.data.title);
+    setbody(me.data.body);
+  };
 
-  useEffect( () => {
-    gettodoInfo()
-    }, []);
-  
+  useEffect(() => {
+    gettodoInfo();
+  }, []);
 
-  
-  async function handleFormSubmit (event){
+  async function handleFormSubmit(event) {
     event.preventDefault();
-    try{
-      await axios.put(`http://localhost:4000/api/v1/todos/${props.theTodo._id}`, { title, body })
-      this.history.push("/ListAll")
-      ;
-    }
-    catch(error){
-      console.log(error)
+    try {
+      await axios.put(
+        `http://localhost:4000/api/v1/todos/${props.theTodo._id}`,
+        { title, body }
+      );
+      props.history.push("/ListAll");
+    } catch (error) {
+      console.log(error);
     }
   }
-  
+
   return (
-        <div>
+    <div>
       <div>
-        <form onSubmit={e => handleFormSubmit(e)}>
-          
-        <label>Title:</label>
-        <input
+        <form onSubmit={(e) => handleFormSubmit(e)}>
+          <label>Title:</label>
+          <input
             type="text"
             name="title"
             value={title}
-            onChange={e => settitle(e.target.value)}
+            onChange={(e) => settitle(e.target.value)}
           />
           <br></br>
           <label>Body:</label>
@@ -50,12 +47,14 @@ function EditTodo(props) {
             type="text"
             name="body"
             value={body}
-            onChange={e => setbody(e.target.value)}
+            onChange={(e) => setbody(e.target.value)}
           />
-          <button type="submit" value="Submit">Edit</button>
+          <button type="submit" value="Submit">
+            Edit
+          </button>
         </form>
       </div>
-      </div>
-    )
+    </div>
+  );
 }
 export default EditTodo;
