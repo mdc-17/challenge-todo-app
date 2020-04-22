35209ab4807910b6d546f306dcc3c8a677050de3
Miguel Dominguez Chamorro <mdchamorro7@gmail.com>
2020-04-22 12:42:04 +0200

    Fixing bugs and working in editTodo

--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -1,48 +1,62 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-class CreateTodo extends Component {
+class EditTodo extends Component {
   constructor(props) {
     super(props);
-    this.state = { title: "", body: "" };
+    this.state = {
+      title: this.props.todo.title,
+      body: this.props.todo.body
+    };
   }
 
   handleFormSubmit = event => {
-    event.preventDefault();
     const title = this.state.title;
     const body = this.state.body;
-    axios
-      .post("http://localhost:4000/api/v1/todos", { title, body })
+
+    event.preventDefault();
+
+    axios.put(`http://localhost:4000/api/v1/todos/${this.props.todo._id}`, 
+    {
+        title,
+        body
+      })
       .then(() => {
-        this.setState({ title: "", body: "" });
-        this.props.history.push("/ListAll")
+        this.props.history.push("/ListAll");
       })
       .catch(error => console.log(error));
   };
 
-  handleChange = event => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
+  handleChangeTitle = event => {
+    this.setState({
+      title: event.target.value
+    });
   };
 
-  
+  handleChangeBody = event => {
+    this.setState({
+      body: event.target.value
+    });
+  };
 
   render() {
     return (
       <div>
+        <hr />
+        <h3>Edit form</h3>
         <form onSubmit={this.handleFormSubmit}>
           <label>Title:</label>
           <input
             type="text"
             name="title"
             value={this.state.title}
-            onChange={e => this.handleChange(e)}
+            onChange={e => this.handleChangeTitle(e)}
           />
           <label>body:</label>
-          <input
+          <textarea
             name="body"
             value={this.state.body}
-            onChange={e => this.handleChange(e)}
+            onChange={e => this.handleChangeBody(e)}
           />
 
           <input type="submit" value="Submit" />
@@ -52,4 +66,4 @@ class CreateTodo extends Component {
   }
 }
 
-export default CreateTodo;
+export default EditTodo;
--- src/components/ListAll.js
+++ src/components/ListAll.js
@@ -1,38 +1,50 @@
-import React, { Component } from 'react'
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 class ListAll extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { listTodo : [] }
-    }
+  constructor(props) {
+    super(props);
+    this.state = { listTodo: [] };
+  }
 
-    getAllTodo = () => {
-        axios.get(`http://localhost:4000/api/v1/todos`)
-        .then(response => {
-            this.setState({listTodo :response.data})
-        })
-    }
+  getAllTodo = () => {
+    axios.get(`http://localhost:4000/api/v1/todos`).then((response) => {
+      this.setState({ listTodo: response.data });
+    });
+  };
 
-    componentDidMount() {
-        this.getAllTodo();
-    }
+  componentDidMount() {
+    this.getAllTodo();
+  }
 
-    render() {
-        return (
-            <div>
-                <div>
-                    {this.state.listTodo.map(eachTodo => {
-                        return (
-                        <div> 
-                                <h1>{eachTodo.title}</h1>
-                                <p>{eachTodo.body}</p>
-                        </div>
-                        )})}
+  handleDelete = (idTodo) => {
+    console.log("ID Todo", idTodo);
+    axios.delete(`http://localhost:4000/api/v1/todos/${idTodo}`);
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          {this.state.listTodo.map((eachTodo) => {
+            return (
+              <div>
+                <Link to={`/todos/${eachTodo._id}`}>
+                  <h1>{eachTodo.title}</h1>
+                </Link>
+                <p>{eachTodo.body}</p>
+                <button onClick={() => this.handleDelete(eachTodo._id)}>
+                  {" "}
+                  Borrar Todo
+                </button>
               </div>
-            </div>
-        )
-    }
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ListAll;
